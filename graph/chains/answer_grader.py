0db321d76314a7da989d5950d9e00afef2cdf179
Mohamed Shaad <98437584+shaadclt@users.noreply.github.com>
2025-08-02 09:42:58 +0530

    hallucinaton_grader

---
+++ graph/chains/answer_grader.py
@@ -0,0 +1,29 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableSequence
+from pydantic import BaseModel, Field
+from model import llm_model
+
+llm = llm_model
+
+class GradeAnswer(BaseModel):
+    
+    binary_score: bool = Field(
+        description = "Answer addresses the question, 'yes' or 'no'"
+    )
+    
+structured_llm_grader = llm.with_structured_output(GradeAnswer)
+
+system = """
+You are a grader assessing whether an answer addresses / resolves a question \n
+Give a binary score 'yes' or 'no'. 'Yes' means that the answer addresses / resolves the question.
+"""
+
+answer_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system",system),
+        ("human", "User question: \n\n {generation} \n\n LLM generation: {generation}"),
+    ]
+)
+
+answer_grader: RunnableSequence = answer_prompt | structured_llm_grader
+
--- graph/chains/hallucination_grader.py
+++ graph/chains/hallucination_grader.py
@@ -13,3 +13,19 @@ class GradeHallucinations(BaseModel):
     binary_score:bool = Field(
         description = "Answer is grounded in the facts, 'yes' or 'no'"
     )
+    
+structured_llm_grader = llm.with_structured_prompt(GradeHallucinations)
+
+system = """
+You are a grader assessing whether an LLM generation is grounded in / supported by a set of retrieved facts. \n
+Give a binary score 'yes' or 'no'. 'Yes' means that the answer is grounded in / supported by the set of facts. 
+"""
+
+hallucination_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system",system),
+        ("human","Set of facts: \n\n {documents} \n\n LLM generation: {generation}"),
+    ]
+)
+
+hallucination_grader: RunnableSequence = hallucination_prompt | structured_llm_grader
